7259807e69a4931ec97be2c05cd7362677ce1b0f
arunkarthikeyan1 <73937726+arunkarthikeyan1@users.noreply.github.com>
2021-01-31 20:33:12 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,29 @@
+class Bob {
+        constructor(x, y, radius) {
+          var options = {
+              'isStatic':false,
+              'restitution':1.0,
+              'friction':0,
+              'density':0.8
+          }
+          this.x=x;
+          this.y=y;
+          this.radius=radius;
+          this.body=Bodies.circle(this.x,this.y,(this.radius)/2,options)
+          World.add(world,this.body);
+         }
+        display(){
+          var pos =this.body.position;
+         
+          push();
+          translate(pos.x, pos.y);
+         
+          rectMode(CENTER);
+          strokeWeight(5);
+          stroke("black");
+          fill(255,204,51);
+          circle(0, 0,this.radius);
+          pop();
+        }
+      }
+      
